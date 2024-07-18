0371d43c4a95e34d29751887eb9f8c9aa43e04a1
mountainboy01 <dharmendrachauhan977@gmail.com>
2024-07-18 19:41:31 +0530

    dropdown-navbar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/********* NAVBAR JS START *********/
+
+const menu = document.getElementById("menu");
+	  const icon = document.getElementById("icon");
+
+	  icon.addEventListener("click",()=>{
+			menu.classList.toggle("active");
+			icon.classList.toggle("fa-times");
+	  });
+
+
+
+
+
+/********* NAVBAR JS START *********/
